3ec73864036769d184147d616eb48f139ff3c90e
sunchengjue <sunchengjue@163.com>
2020-11-12 16:34:52 +0800

    bounce
    第一次提交
    
    Co-Authored-By: sunchengjue1996 <43510300+sunchengjue1996@users.noreply.github.com>

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+import math
+h = 100
+bounce = 0
+for i in range(10):
+    h = h * (3/5)
+    bounce = bounce + 1 
+    print("第%s次，弹起第高度是%s" % (bounce,h))
+
+# print(100 * (3/5))
---
+++ hello.py
@@ -0,0 +1,1 @@
+print(helleo world)
